fdd781f885a991bfce5c6988272ded641f161692
Nate Huff <nah0027@mix.wvu.edu>
2022-02-03 02:20:12 -0500

    I tried

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/features/features.ts
@@ -0,0 +1,11 @@
+import{Component} from "@angular/core";
+
+@Component({
+    selector: 'app-features',
+    templateUrl: 'features.html',
+    styleUrls: ['features.css']
+})
+
+export class FeaturesComponent{
+    
+}
